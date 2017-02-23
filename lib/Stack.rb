787878fb8e5fc9d9b2f3f62a172ebb5b8761e8ac
Regan Huff <regan.huff@gmail.com>
2017-02-23 12:05:37 -0800

    Add Stack implementation

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,23 @@ class Stack
   end
 
   def push(element)
+    @store << element
   end
 
   def pop
+    @store.pop
   end
 
   def top
+    @store[-1]
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    size == 0
   end
 
   def to_s
