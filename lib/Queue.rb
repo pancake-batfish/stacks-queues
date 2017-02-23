671372e3f07c5b517309a6e147f9732f1bdecd07
Regan Huff <regan.huff@gmail.com>
2017-02-23 12:17:50 -0800

    Add Queue implementation

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    @store.first
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    size == 0
   end
 
   def to_s
+    return @store.to_s
   end
 end
